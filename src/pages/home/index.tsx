a0eed5106d7093c1e8585553cb640787b1396a27
serendipity-seeker <valkyrie.prim@gmail.com>
2025-01-28 02:21:13 +0900

    refactor: organize file structures

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -6,13 +6,13 @@ import History from "./components/History";
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col gap-4">
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-3 gap-4 min-h-[680px]">
         <Chart className="col-span-2" />
         <Orderbook className="col-span-1" />
       </div>
-      <div className="grid grid-cols-3 gap-4">
-        <OrderForm className="col-span-2" />
-        <History className="col-span-1" />
+      <div className="grid grid-cols-3 gap-4 min-h-[320px]">
+        <History className="col-span-2" />
+        <OrderForm className="col-span-1" />
       </div>
     </div>
   );
---
+++ src/types/tx.types.ts
@@ -0,0 +1,101 @@
+export type TickInfo = {
+  tick: number;
+  duration: number;
+  epoch: number;
+  initialTick: number;
+};
+
+export type Balance = {
+  id: string;
+  balance: number;
+  validForTick: number;
+  latestIncomingTransferTick: number;
+  latestOutgoingTransferTick: number;
+  incomingAmount: number;
+  outgoingAmount: number;
+  numberOfIncomingTransfers: number;
+  numberOfOutgoingTransfers: number;
+};
+
+export interface IQuerySC {
+  contractIndex: number;
+  inputType: number;
+  inputSize: number;
+  requestData: string;
+}
+
+export interface IQuerySCResponse {
+  responseData: string;
+}
+
+export interface IEndedStakeStatus {
+  unLockedAmount: number;
+  rewardedAmount: number;
+  status?: boolean;
+}
+
+export interface TxStatus {
+  txId: string;
+  moneyFlew: boolean;
+}
+
+export interface LatestStats {
+  timestamp: string;
+  circulatingSupply: string;
+  activeAddresses: number;
+  price: number;
+  marketCap: string;
+  epoch: number;
+  currentTick: number;
+  ticksInCurrentEpoch: number;
+  emptyTicksInCurrentEpoch: number;
+  epochTickQuality: number;
+  burnedQus: string;
+}
+
+export interface RichList {
+  pagination: {
+    totalRecords: number;
+    currentPage: number;
+    totalPages: number;
+  };
+  epoch: number;
+  richList: {
+    entities: { identity: string; balance: string }[];
+  };
+}
+
+export interface Transaction {
+  sourceId: string;
+  destId: string;
+  amount: string;
+  tickNumber: number;
+  inputType: number;
+  inputSize: number;
+  inputHex: string;
+  signatureHex: string;
+  txId: string;
+}
+export interface TxHistory {
+  transactions: {
+    tickNumber: number;
+    identity: string;
+    transactions: {
+      transaction: Transaction;
+      timestamp: string;
+      moneyFlew: boolean;
+    }[];
+  }[];
+}
+
+export interface EpochTicks {
+  pagination: {
+    totalRecords: number;
+    currentPage: number;
+    totalPages: number;
+    pageSize: number;
+    nextPage: number;
+    previousPage: number;
+  };
+  ticks: { tickNumber: number; isEmpty: boolean }[];
+}

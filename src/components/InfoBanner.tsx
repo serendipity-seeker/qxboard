32e1383fc9808c55dcd72b9a2781942e23b953d1
serendipity-seeker <valkyrie.prim@gmail.com>
2025-02-02 00:28:50 +0900

    feat: add asset selector, order form

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -1,8 +1,37 @@
+import { useState } from "react";
+import AccountSelector from "./ui/AccountSelector";
+import { ISSUER } from "@/constants";
+import { actionAtom } from "@/store/action";
+import { useAtom } from "jotai";
+
+const assetOptions = Array.from(ISSUER.entries()).map(([key, value]) => ({
+  label: key,
+  value: value,
+}));
+
 const InfoBanner: React.FC = () => {
+  const [selectedAsset, setSelectedAsset] = useState(0);
+  const [action, setAction] = useAtom(actionAtom);
+
+  const handleAssetChange = (value: number) => {
+    setSelectedAsset(value);
+    setAction({
+      ...action,
+      curPair: assetOptions[value].label,
+    });
+  };
+
   return (
-    <div className="p-4">
-      <div className="flex justify-between">
-        <div className="">InfoBanner</div>
+    <div className="p-2">
+      <div className="flex items-center justify-between">
+        <div className="w-[300px]">
+          <AccountSelector
+            options={assetOptions}
+            selected={selectedAsset}
+            setSelected={handleAssetChange}
+            showValue={false}
+          />
+        </div>
         <div className="">InfoBanner</div>
       </div>
     </div>

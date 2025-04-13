a55632387006ea7e7ca96548550a8b486db40a41
serendipity-seeker <valkyrie.prim@gmail.com>
2025-04-13 14:45:19 +0100

    chore: change test api

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
-export const RPC_URL = "https://rpc.qubicbay.io";
+export const RPC_URL = "https://rpc.qubic.org";
 
-export const API_URL = "https://api.qubicbay.io";
+export const API_URL = "https://api.qubic.org";
 
 export const EXPLORER_URL = "https://explorer.qubic.org";
 

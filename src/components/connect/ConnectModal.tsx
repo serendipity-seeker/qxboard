ed34a0c8680c40b869c853ff69073ff5b4cfd5e4
serendipity-seeker <valkyrie.prim@gmail.com>
2025-04-15 06:04:11 +0100

    feat: remove seed connect, vault connect

--- src/components/connect/ConnectModal.tsx
+++ src/components/connect/ConnectModal.tsx
@@ -179,7 +179,7 @@ const ConnectModal = ({ open, onClose, darkMode }: { open: boolean; onClose: () 
                           <img src={WalletConnectLogo} alt="Wallet Connect Logo" className="h-8 w-8" />
                           <span className="w-32">Wallet Connect</span>
                         </Button>
-                        <div className="my-2 flex w-full items-center justify-center">
+                        {/* <div className="my-2 flex w-full items-center justify-center">
                           <div className="flex-grow border-t border-gray-300"></div>
                           <span className="px-4 text-red-500">
                             <AlertTriangle className="mr-2 inline-block" /> BE CAREFUL!
@@ -191,7 +191,7 @@ const ConnectModal = ({ open, onClose, darkMode }: { open: boolean; onClose: () 
                         </Button>
                         <Button variant="default" onClick={() => setSelectedMode("vault-file")}>
                           Vault File
-                        </Button>
+                        </Button> */}
                       </>
                     )}
                   </motion.div>
